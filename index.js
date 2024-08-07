9e01495138cbc78dff010cbe7eb06573122f3b3a
Nikita Vlasuk <niksvlas@yandex.ru>
2024-08-07 12:02:04 +0300

    count8

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const http = require('http');
 
+var http = require('http');
+var url = require('url');
+const { parse } = require(querystring);
 http.createServer(function (request, response) {
-    response.setHeader('UserId', 12);
-    response.setHeader(
-        'Content-Type',
-        'text/html; charset=utf-8;'
+    console.log('Url: ' + request.url);
+    console.log('Тип запроса: ' + request.method);
+    console.log(
+        'User-Agent: ' + request.headers['user-agent']
     );
+    console.log('Все заголовки');
+    let body = '';
+    request.on('data', chunk => {
+        body += chunk.toString();
+    });
+    request.on('end', ()=>{
+        console.log(body);
+    });
+
     response.write('Дратути');
     response.end();
 }).listen(3000);
